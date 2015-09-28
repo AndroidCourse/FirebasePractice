9dedfbac8e86faed0f403acf8adcd652f5bf3f98
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-09-28 13:16:55 -0400

    added imaze from firebase to android

--- src/com/sukhpreet/sampleprojecttwo/MainLandingPage.java
+++ src/com/sukhpreet/sampleprojecttwo/MainLandingPage.java
@@ -1,9 +1,16 @@
 package com.sukhpreet.sampleprojecttwo;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainLandingPage extends Activity {
 
@@ -11,6 +18,30 @@ public class MainLandingPage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_landing_page);
+
+		// Fire Base
+		Firebase.setAndroidContext(this);
+		// Fetching Image from Cloud
+		final ImageView imgLogo = (ImageView) findViewById(R.id.imageView1);
+		Firebase fImageMainLandingPage = new Firebase("https://amber-heat-1015.firebaseio.com/Students/-JzGGvEujf_RwLyeIPSZ/imaze");
+		fImageMainLandingPage.addValueEventListener(new ValueEventListener() {
+
+			@Override
+			public void onDataChange(DataSnapshot arg0) {
+				// TODO Auto-generated method stub
+				String tempImage = (String) arg0.getValue();
+				// Assigning Image to Image view
+			
+				UrlImageViewHelper.setUrlDrawable(imgLogo, tempImage);
+			}
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 	}
 
 	@Override
